dd95d81732c5698907a2e4606570ff675bb48c62
Anthony Guiri <exastir@icloud.com>
2022-12-02 23:19:08 +0100

    fix(main): remove unused function

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,16 +6,11 @@
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 14:16:37 by aguiri            #+#    #+#             */
-/*   Updated: 2022/12/02 22:45:22 by aguiri           ###   ########.fr       */
+/*   Updated: 2022/12/02 23:18:55 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-void	stop_simulation(t_var *var)
-{
-	exit (EXIT_SUCCESS);
-}
 
 int	main(int argc, char **argv)
 {
